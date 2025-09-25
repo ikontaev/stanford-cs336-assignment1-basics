992b6779f44e59ed10d16361ddce333d0baaafd8
ikontaev <ideis@protonmail.com>
2025-09-25 17:04:35 +0200

    Fix vocab and merges saving

--- tokenization/utils.py
+++ tokenization/utils.py
@@ -18,7 +18,7 @@ def save_vocab(path: str, vocab: dict[int, bytes]):
         json.dump(vocab_inv, f, indent=4, ensure_ascii=False)
 
 
-def save_merges(path: str, merges: dict[tuple[int, int], int], vocab: dict[int, bytes]):
+def save_merges(path: str, merges: list[tuple[bytes, bytes]]):
     with open(path, "w") as f:
-        for i, (token1, token2) in enumerate(merges):
-            f.write(f"{i}: {bytes_to_str(vocab[token1])} {bytes_to_str(vocab[token2])}\n")
+        for token1, token2 in merges:
+            f.write(f"{bytes_to_str(token1)} {bytes_to_str(token2)}\n")
